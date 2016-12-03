3d6e8b41e3321a753ea3da646cf9370b6b466f7e
Fiona Ballard <fionaballard@Fiona-2.local>
2016-12-02 19:26:44 -0500

    sequelize

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,17 @@
 var mysql = require('mysql');
-var connection = mysql.createConnection({
+var connection;
+
+if(process.env.JAWSDB_URL){
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+
+}else{
+	connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '',
-    database: 'burgers_db'
+    password: 'root',
+    database: 'burger_db'
 });
+};
 
 
 connection.connect(function(err) {
